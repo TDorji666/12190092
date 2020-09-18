00867f4a0a2bf6b90b2fe59aabfda77147d27e44
unknown <12190092.gcit@rub.edu.bt>
2020-09-19 00:45:11 +0600

    First Folder

---
+++ ARRAY/array.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class array{
+	public static void main(String[] args) {
+		int x[]={1,2,4,6,7};
+		int y=x.length;
+		//for (int i=0;i<x.length;i++) {
+			System.out.println("The number is : " + Arrays.toString(x));
+			
+		//}																									
+		System.out.println("The array size is : " + y);
+
+		                                                                                                  
+	}
+}
